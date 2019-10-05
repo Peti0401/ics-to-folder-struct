84532e8e83d9ccf1517f9d0302c80bf7c66f18cd
Péter Gyarmati <gyarmatipeti01@gmail.com>
2019-10-05 21:30:49 +0200

    Functionalities refactored into methods

--- main.py
+++ main.py
@@ -2,27 +2,34 @@ import os
 import sys
 from event import Event
 
-# Reading the ics file and saving its lines into a list
-with open("/Users/gyarmatip/Downloads/ww(1).ics") as file:
-    lines = file.read().splitlines()
 
-# A list which will hold groups of lines
-line_groups = []
-# Indices to track the beginning and end of the given events
-begin_index = 0
-end_index = 0
+def read_file(file_path: str) -> list:
+    with open(file_path) as file:
+        lines = file.read().splitlines()
+    return lines
 
-# Populating grouped_lines list
-for index, line in enumerate(lines):
-    if line.__contains__("BEGIN:VEVENT"):  # Beginning of event found
-        begin_index = index
-    elif line.__contains__("END:VEVENT"):  # End of event found
-        end_index = index
-        line_groups.append(lines[begin_index:end_index+1])
 
-events = []
-# constructing event objects from the line groups
-for line_group in line_groups:
+# Returns a list of lists which include the lines of an event
+def group_lines(ics_file_lines: list) -> list:
+    # A list which will hold groups of lines
+    line_groups = []
+
+    # Indices to track the beginning and end of the given events
+    begin_index = 0
+    end_index = 0
+
+    # Populating grouped_lines list
+    for index, line in enumerate(ics_file_lines):
+        if line.__contains__("BEGIN:VEVENT"):  # Beginning of event found
+            begin_index = index
+        elif line.__contains__("END:VEVENT"):  # End of event found
+            end_index = index
+            line_groups.append(ics_file_lines[begin_index:end_index+1])
+    return line_groups
+
+
+# Constructs a single event object from a line group, which holds all the attribute name - value pairs
+def construct_single_event(line_group: list) -> Event:
     event = Event()
     for line in line_group:
         # splitting the line at : to extract the current attribute's name and value
@@ -34,25 +41,42 @@ for line_group in line_groups:
         attr_value = key_value[1]
         # setting the attribute value reflectively
         setattr(event, attr_name, attr_value)
-    events.append(event)
+    return event
 
-root_path = '/Users/gyarmatip/Documents/Coding/test-env/python-mkdirs'
-# sub-directories can be specified
-sub_dirs = ['s1', 's1/ss1', 's2', 's2/ss2']
 
-# Actually creating folders
-for event in events:
-    # Formatting a date from 20191129T084500Z to 2019_11_19
-    dir_name = '{}_{}_{}'.format(
-        event.dtstart[0:4], event.dtstart[4:6], event.dtstart[6:8])
-    dir_path = '{}/{}'.format(root_path, dir_name)
-    print(dir_path)
-    # Making the base directory
-    # os.makedirs(dir_path)
-    # Making all sub directories if specified
-    print()
-    for sub_dir in sub_dirs:
-        sub_dir_path = '{}/{}'.format(dir_path, sub_dir)
-        print(sub_dir_path)
-        # os.makedirs(sub_dir_path)
-    print()
+# Constructs a list of events from multiple line groups
+def construct_events(line_groups: list) -> list:
+    events = []
+    # constructing event objects from the line groups
+    for line_group in line_groups:
+        event = construct_single_event(line_group)
+        events.append(event)
+    return events
+
+
+def construct_directories(ics_file_lines: list, root_dir_path: str, sub_dirs: list):
+    line_groups = group_lines(ics_file_lines)
+    events = construct_events(line_groups)
+    for event in events:
+        # Formatting a date from 20191129T084500Z to 2019_11_19
+        dir_name = format_event_date(event.dtstart)
+        dir_path = '{}/{}'.format(root_dir_path, dir_name)
+        # Making the base directory
+        # os.makedirs(dir_path)
+        # Making all sub directories if specified
+        for sub_dir in sub_dirs:
+            sub_dir_path = '{}/{}'.format(dir_path, sub_dir)
+            print(sub_dir_path)
+            # os.makedirs(sub_dir_path)
+
+
+def format_event_date(date: str) -> str:
+    return '{}_{}_{}'.format(date[0:4], date[4:6], date[6:8])
+
+
+# Main
+ics_file_path = '/Users/gyarmatip/Downloads/ww(1).ics'
+ics_file_lines = read_file(ics_file_path)
+root_dir_path = '/Users/gyarmatip/Documents/Coding/test-env/python-mkdirs'
+sub_dirs = ['Literatur', 'Folien']
+construct_directories(ics_file_lines, root_dir_path, sub_dirs)
